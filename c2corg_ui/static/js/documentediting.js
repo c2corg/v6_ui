e58b106a3b78f109f721ffb9d5c2aee7ed3a73d6
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2015-10-29 10:11:45 +0100

    Add documentation to the appDocumentEditing directive

--- c2corg_ui/static/js/documentediting.js
+++ c2corg_ui/static/js/documentediting.js
@@ -6,9 +6,21 @@ goog.require('ol.format.GeoJSON');
 goog.require('ol.geom.Point');
 
 
-
 /**
- * @constructor
+ * Provides the "appDocumentEditing" directive, which applies to the forms
+ * used to create a new document (whatever its type) or to edit an existing
+ * one. The fields and validations are set and performed in the form itself.
+ *
+ * Example:
+ * <form app-document-editing="waypoints" app-document-editing-model="waypoint"
+ *   app-document-editing-id="42" app-document-editing-culture="fr"
+ *   name="editForm" novalidate
+ *   ng-submit="editCtrl.submitForm(editForm.$valid)">
+ *
+ * The main directive attribute contains the resource name (eg. "waypoints").
+ * Additional attributes are used to specify the model name and, optionally,
+ * the document id and culture.
+ *
  * @return {angular.Directive} The directive specs.
  * @ngInject
  */
