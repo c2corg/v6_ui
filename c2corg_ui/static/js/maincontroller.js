7b5957aed66b74e96611ef04334938a5add4b60c
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2015-11-17 17:11:55 +0100

    i18n for dropdowns

--- c2corg_ui/static/js/maincontroller.js
+++ c2corg_ui/static/js/maincontroller.js
@@ -5,13 +5,14 @@ goog.require('app');
 
 
 /**
+ * @param {angular.Scope} $rootScope The rootScope provider.
  * @param {angularGettext.Catalog} gettextCatalog Gettext catalog.
  * @param {string} langUrlTemplate Language URL template.
  * @constructor
  * @export
  * @ngInject
  */
-app.MainController = function(gettextCatalog, langUrlTemplate) {
+app.MainController = function($rootScope, gettextCatalog, langUrlTemplate) {
 
   /**
    * @type {angularGettext.Catalog}
@@ -30,6 +31,17 @@ app.MainController = function(gettextCatalog, langUrlTemplate) {
    * @export
    */
   this.lang;
+
+  /**
+   * Put angular-gettext's translate function on the scope to translate
+   * ng-options based dropdown lists. See
+   * https://github.com/rubenv/angular-gettext/issues/58
+   * @param {string} str String to translate.
+   * @return {string} Translated string.
+   */
+  $rootScope['translate'] = function(str) {
+    return gettextCatalog.getString(str);
+  };
 
   this.switchLanguage('fr');
 };
