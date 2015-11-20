92a985cdaa28eda3ab0f99e8d637ec7adf18213e
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2015-11-20 20:58:55 +0100

    Fix dropdown translation

--- c2corg_ui/static/js/maincontroller.js
+++ c2corg_ui/static/js/maincontroller.js
@@ -5,11 +5,28 @@ goog.require('app');
 
 
 /**
+ * @param {angularGettext.Catalog} gettextCatalog Gettext catalog.
  * @constructor
  * @export
  * @ngInject
  */
-app.MainController = function() {
+app.MainController = function(gettextCatalog) {
+
+  /**
+   * @type {angularGettext.Catalog}
+   * @private
+   */
+  this.gettextCatalog_ = gettextCatalog;
+};
+
+
+/**
+ * @param {string} str String to translate.
+ * @return {string} Translated string.
+ * @export
+ */
+app.MainController.prototype.translate = function(str) {
+  return this.gettextCatalog_.getString(str);
 };
 
 
