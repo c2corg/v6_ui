d9b240fdd962d03c02058f44bcfec9231d348959
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2015-12-14 14:44:41 +0100

    Add archive pages + some improvements in history page

--- c2corg_ui/views/route.py
+++ c2corg_ui/views/route.py
@@ -23,13 +23,22 @@ class Route(Document):
 
     @view_config(route_name='routes_view',
                  renderer='c2corg_ui:templates/route/view.html')
+    @view_config(route_name='routes_archive',
+                 renderer='c2corg_ui:templates/route/view.html')
     def view(self):
         id, culture = self._validate_id_culture()
-        route, locale = self._get_document(id, culture)
+        if 'version' in self.request.matchdict:
+            version_id = int(self.request.matchdict['version'])
+            route, locale, version = self._get_archived_document(
+                id, culture, version_id)
+        else:
+            route, locale = self._get_document(id, culture)
+            version = None
         self.template_input.update({
             'culture': culture,
             'route': route,
-            'locale': locale
+            'locale': locale,
+            'version': version
         })
         return self.template_input
 
