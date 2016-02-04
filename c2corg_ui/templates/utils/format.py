6587f12b949f0ff5483a673752c1bcde243953e6
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2016-02-04 13:03:21 +0100

    Add support of formatting to various text attributes in list/view/diff/history pages

--- c2corg_ui/templates/utils/format.py
+++ c2corg_ui/templates/utils/format.py
@@ -7,7 +7,10 @@ def sanitize(text):
     return html.escape(text)
 
 
-def parse_code(text):
-    text = markdown.markdown(text)
-    bbcode_parser = bbcode.Parser(escape_html=False, newline='\n')
-    return bbcode_parser.format(text)
+def parse_code(text, md=True, bb=True):
+    if md:
+        text = markdown.markdown(text, output_format='xhtml5')
+    if bb:
+        bbcode_parser = bbcode.Parser(escape_html=False, newline='\n')
+        text = bbcode_parser.format(text)
+    return text
