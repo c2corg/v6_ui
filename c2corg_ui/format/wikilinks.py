a496b4c9c338d8f16d048d673b726856d9e80d51
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2016-02-11 19:36:23 +0100

    Add Markdown extension for c2corg wikilinks

---
+++ c2corg_ui/format/wikilinks.py
@@ -0,0 +1,45 @@
+'''
+c2corg wikiLinks Extension for Python-Markdown
+==============================================
+
+Converts tags such as [[document_type/document_id|label]]
+to <a href="/document_type/document_id">label</a>
+
+Inspired from https://github.com/waylan/Python-Markdown/blob/master
+/markdown/extensions/wikilinks.py
+'''
+
+from markdown.extensions import Extension
+from markdown.inlinepatterns import Pattern
+from markdown.util import etree
+
+
+class C2CWikiLinkExtension(Extension):
+
+    def extendMarkdown(self, md, md_globals):  # noqa
+        self.md = md
+
+        wikilink_re = r'\[\[([^|]+)\|([^\]]+)\]\]'
+        pattern = C2CWikiLinks(wikilink_re)
+        pattern.md = md
+        # append to end of inline patterns
+        md.inlinePatterns.add('c2cwikilink', pattern, "<not_strong")
+
+
+class C2CWikiLinks(Pattern):
+
+    def handleMatch(self, m):  # noqa
+        if m.group(2).strip():
+            url = m.group(2).strip()
+            url = '/' + url if url[0] != '/' else url
+            label = m.group(3).strip()
+            a = etree.Element('a')
+            a.text = label
+            a.set('href', url)
+        else:
+            a = ''
+        return a
+
+
+def makeExtension(*args, **kwargs):  # noqa
+    return C2CWikiLinkExtension(*args, **kwargs)
--- c2corg_ui/templates/utils/format.py
+++ c2corg_ui/templates/utils/format.py
@@ -2,6 +2,8 @@ import bbcode
 import markdown
 import html
 
+from c2corg_ui.format.wikilinks import C2CWikiLinkExtension
+
 
 def sanitize(text):
     return html.escape(text)
@@ -9,7 +11,9 @@ def sanitize(text):
 
 def parse_code(text, md=True, bb=True):
     if md:
-        text = markdown.markdown(text, output_format='xhtml5')
+        wikilink = C2CWikiLinkExtension()
+        text = markdown.markdown(text, output_format='xhtml5',
+            extensions=[wikilink])
     if bb:
         bbcode_parser = bbcode.Parser(escape_html=False, newline='\n')
         text = bbcode_parser.format(text)
