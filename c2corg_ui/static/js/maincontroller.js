196f923bfaed13c202b02b1c00072a1904f2a7a6
Adam Gina <adam.gina@camptocamp.com>
2016-02-12 13:31:52 +0100

    add new js to main + path function

--- c2corg_ui/static/js/maincontroller.js
+++ c2corg_ui/static/js/maincontroller.js
@@ -47,3 +47,25 @@ app.MainController.prototype.page_title = function(title) {
 };
 
 app.module.controller('MainController', app.MainController);
+
+/**
+ * @param {string} path
+ * @export
+ * function returning true if the window.location.pathname contains
+ * the path parameter. Only exceptions are 'topoguide', where all
+ * kinds of documents are associated, and '/' (home).
+ * TODO : add an array of possible document types and make a for loop
+ */
+app.MainController.prototype.isPath = function(path) {
+  var location = window.location.pathname;
+  // path = '/'
+  if (path === location) {
+    return 'home';
+  // if topoguide, it can be all kinds of documents
+  } else if (path === 'topoguide') {
+    return location.indexOf('waypoints') > -1 || location.indexOf('routes') > -1
+  } else {
+    return location.indexOf(path) > -1;
+  }
+};
+
