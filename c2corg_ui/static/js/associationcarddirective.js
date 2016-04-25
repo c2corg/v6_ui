11b1e4263b204d636681a880a2be4a2559e64c75
Adam Gina <adam.gina@camptocamp.com>
2016-04-25 12:41:54 +0200

    updated card HTMLs + partials cards

--- c2corg_ui/static/js/associationcarddirective.js
+++ c2corg_ui/static/js/associationcarddirective.js
@@ -4,10 +4,41 @@ goog.require('app.utils');
 
 
 /**
+ * @param {angular.$compile} $compile Angular compile service.
+ * @ngInject
  * @return {angular.Directive} Directive Definition Object.
  */
-app.associationCardDirective = function() {
+app.associationCardDirective = function($compile) {
   var url = app.utils.buildDocumentUrl('{{::doc.documentType}}', '{{::doc.document_id}}', '{{::doc.locales[0].lang}}');
+
+  // You should take the example from templates/{documentType}/card.html
+  var template = function(doc) {
+    // WP
+    if (doc['waypoint_type']) {
+      return '<a ng-href="' + url + '"> <span class="list-item-title">{{::doc.locales[0].title}}</span>' +
+                  '<span class="list-item-info">' +
+                    '<p><b  class="value-title" translate>elevation</b>: <span class="value">{{::doc.elevation}}m</span></p>' +
+                        '<span class="waypoint-type icon-{{::doc.waypoint_type}}"></span>' +
+                        '<span x-translate>{{::doc.waypoint_type}}</span>' +
+                  '</span>' +
+                 '</a>' +
+                 '<app-delete-association parent-id="::parentId" child-id="::doc.document_id" added-documents="addedDocuments">' +
+                 '</app-delete-association>';
+
+    // Route
+    } else if (doc['activities']) {
+      return '<a ng-href="' + url + '"> <span class="list-item-title">{{::doc.locales[0].title}}</span>' +
+                    '<div class="list-item-info">' +
+                      '<p ng-if="doc.elevation_max"><b  class="value-title" translate>elevation</b>: <span class="value">{{::doc.elevation_max}}m</span></p>' +
+                      '<div class="route-activities">' +
+                        '<span ng-repeat="activity in doc.activities" class="route-activity icon-{{activity}}" uib-tooltip="{{ mainCtrl.translate(activity) }}" tooltip-placement="right"></span>' +
+                      '</div>' +
+                    '</div>' +
+                  '</a>' +
+                  '<app-delete-association parent-id="::parentId" child-id="::doc.document_id" added-documents="addedDocuments">' +
+                  '</app-delete-association>';
+    }
+  }
   return {
     restrict: 'E',
     scope: {
@@ -15,7 +46,10 @@ app.associationCardDirective = function() {
       'doc': '=',
       'addedDocuments': '='
     },
-    template: '<a ng-href="' + url + '">{{::doc.locales[0].title}}</a> <app-delete-association parent-id="::parentId" child-id="::doc.document_id" added-documents="addedDocuments"></app-delete-association>'
+    link: function(scope, element, attrs, ctrl) {
+      element.html(template(scope['doc']));
+      $compile(element.contents())(scope);
+    }
   };
 };
 
