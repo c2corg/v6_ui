92dec2a9b58e6484f28a4093824d3a9c2bd90326
Adam Gina <adam.gina@camptocamp.com>
2016-05-10 14:27:25 +0200

    confirm del-association modal

--- c2corg_ui/static/js/deleteassociation.js
+++ c2corg_ui/static/js/deleteassociation.js
@@ -1,6 +1,6 @@
 goog.provide('app.DeleteAssociationController');
 goog.provide('app.deleteAssociationDirective');
-
+goog.provide('app.DeleteAssociationModalController');
 goog.require('app.Api');
 
 
@@ -10,7 +10,7 @@ goog.require('app.Api');
 app.deleteAssociationDirective = function() {
   return {
     restrict: 'E',
-    controller: 'AppDeleteAssociationController as ctrl',
+    controller: 'AppDeleteAssociationController as unassociateCtrl',
     bindToController: {
       'parentId': '=',
       'childId': '=',
@@ -18,7 +18,12 @@ app.deleteAssociationDirective = function() {
     },
     link: function(scope, element, attrs, controller) {
       $(element).on('click', function() {
-        controller.unassociateDocument_(element[0]);
+        var modal = controller.openModal_();
+        modal.result.then(function(res) {
+          if (res) {
+            controller.unassociateDocument_(element[0]);
+          }
+        });
       });
     },
     template: '<span class="glyphicon glyphicon-trash"></span>'
@@ -30,11 +35,34 @@ app.module.directive('appDeleteAssociation', app.deleteAssociationDirective);
 
 /**
  * @constructor
+ * @param {!angular.Scope} $scope Scope.
  * @param {app.Api} appApi The API service
+ * @param {angular.$compile} $compile Angular compile service.
+ * @param {Object} $uibModal modal from angular bootstrap
  * @param {angular.Scope} $rootScope
  * @ngInject
  */
-app.DeleteAssociationController = function(appApi, $attrs, $rootScope) {
+app.DeleteAssociationController = function(appApi, $attrs, $rootScope, $uibModal, $compile, $scope) {
+
+  /**
+   * @type {angular.$compile}
+   * @private
+   */
+  this.compile_ = $compile;
+
+
+  /**
+   * @type {!angular.Scope}
+   * @private
+   */
+  this.scope_ = $scope;
+
+
+  /**
+   * @type {Object} angular bootstrap modal
+   * @private
+   */
+  this.modal_ = $uibModal;
 
 
   /**
@@ -83,6 +111,53 @@ app.DeleteAssociationController = function(appApi, $attrs, $rootScope) {
 
 };
 
+/**
+ * We have to use a secondary controller for the modal so that we can inject
+ * uibModalInstance which is not available from the first level controller.
+ * @param {Object} $uibModalInstance modal from angular bootstrap
+ * @constructor
+ * @ngInject
+ * @returns {app.DeleteAssociationModalController}
+ */
+app.DeleteAssociationModalController = function($uibModalInstance) {
+  /**
+   * @type {Object} $uibModalInstance angular bootstrap
+   * @private
+   */
+  this.modalInstance_ = $uibModalInstance;
+};
+
+app.module.controller('AppDeleteAssociationModalController', app.DeleteAssociationModalController);
+
+
+/**
+ * @export
+ */
+app.DeleteAssociationModalController.prototype.close = function(action) {
+  this.modalInstance_.close(action);
+};
+
+
+/**
+ * @export
+ */
+app.DeleteAssociationModalController.prototype.dismiss = function() {
+  this.modalInstance_.close();
+};
+
+
+/**
+ * @private
+ */
+app.DeleteAssociationController.prototype.openModal_ = function() {
+  var template = $('#delete-association-modal').clone();
+  return this.modal_.open({
+    animation: true,
+    size: 'sm',
+    template: this.compile_(template)(this.scope_),
+    controller: 'AppDeleteAssociationModalController as delModalCtrl'
+  });
+};
 
 /**
  * Unassociate the document and remove the card, either by splicing the array
