66fd1ccd8d832a947ed14e853401a3bf1c402b15
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2016-06-24 12:15:19 +0200

    Add support of string values in search filter sliders

--- c2corg_ui/static/js/search/slider.js
+++ c2corg_ui/static/js/search/slider.js
@@ -19,20 +19,21 @@ app.sliderDirective = function() {
     bindToController: true,
     scope: {
       'filter': '@',
-      'filtersList': '='
+      'filtersList': '=',
+      'valuesList': '='
     },
     template:
         '<input type="range" class="range-between" data-slider-tooltip="hide">' +
         '<span class="slider-min-max">' +
-          '<p class="min-value">{{sliderCtrl.min}} <span x-translate>{{sliderCtrl.unit}}</span></p>' +
-          '<p class="max-value">{{sliderCtrl.max}} <span x-translate>{{sliderCtrl.unit}}</span></p>' +
+          '<p class="min-value">{{sliderCtrl.dispMin}} <span x-translate>{{sliderCtrl.unit}}</span></p>' +
+          '<p class="max-value">{{sliderCtrl.dispMax}} <span x-translate>{{sliderCtrl.unit}}</span></p>' +
         '</span>',
     link: function(scope, el, attr, ctrl) {
       el.find('.range-between').slider({
         min: ctrl.boundaries[0],
         max: ctrl.boundaries[1],
         value: [ctrl.min, ctrl.max],
-        step: parseInt(attr['step'], 10) || app.SliderController.STEP
+        step: ctrl.step
       });
     }
   };
@@ -72,23 +73,50 @@ app.SliderController = function($scope, $element, $attrs, ngeoLocation) {
   this.location_ = ngeoLocation;
 
   /**
-   * @type {Array.<number>}
-   * @const
+   * @type {?Array.<string>}
+   * @export
    */
-  this.boundaries = [
-    parseInt($attrs['min'], 10) || app.SliderController.MIN,
-    parseInt($attrs['max'], 10) || app.SliderController.MAX
-  ];
+  this.valuesList;
+
+  /**
+   * @type {boolean}
+   * @private
+   */
+  this.showStringValues_ = this.valuesList !== undefined &&
+    this.valuesList.length > 0;
+
+  /**
+   * @type {Array.<number>}
+   * @public
+   */
+  this.boundaries;
 
   /**
    * @type {number}
-   * @export
+   * @public
+   */
+  this.step;
+
+  if (this.showStringValues_) {
+    this.boundaries = [0, this.valuesList.length - 1];
+    this.step = 1;
+  } else {
+    this.boundaries = [
+      parseInt($attrs['min'], 10) || app.SliderController.MIN,
+      parseInt($attrs['max'], 10) || app.SliderController.MAX
+    ];
+    this.step = parseInt($attrs['step'], 10) || app.SliderController.STEP;
+  }
+
+  /**
+   * @type {number}
+   * @public
    */
   this.min = this.boundaries[0];
 
   /**
    * @type {number}
-   * @export
+   * @public
    */
   this.max = this.boundaries[1];
 
@@ -116,11 +144,22 @@ app.SliderController = function($scope, $element, $attrs, ngeoLocation) {
    */
   this.sliderFirst_ = true;
 
+  /**
+   * @type {number|string}
+   * @export
+   */
+  this.dispMin = this.getDisp_(this.min);
+
+  /**
+   * @type {number|string}
+   * @export
+   */
+  this.dispMax = this.getDisp_(this.max);
+
   this.getRangeFromUrl_();
 
   this.element_.on('slide', function(event) {
-    this.min = event.value[0];
-    this.max = event.value[1];
+    this.updateMinMax_(event.value);
     this.scope_.$apply();
   }.bind(this));
 
@@ -140,11 +179,18 @@ app.SliderController.prototype.getRangeFromUrl_ = function() {
     if (range.length != 2) {
       return;
     }
-    range = range.map(function(x) {
-      return parseInt(x, 10);
-    });
-    this.min = range[0];
-    this.max = range[1];
+    if (this.showStringValues_) {
+      var min = this.valuesList.indexOf(range[0]);
+      var max = this.valuesList.indexOf(range[1]);
+      if (min !== -1 && max !== -1) {
+        this.updateMinMax_([min, max]);
+      }
+    } else {
+      range = range.map(function(x) {
+        return parseInt(x, 10);
+      });
+      this.updateMinMax_(range);
+    }
   }
 };
 
@@ -162,7 +208,7 @@ app.SliderController.prototype.handleRangeChange_ = function() {
       delete this.filtersList[this.filter];
       this.location_.deleteParam(this.filter);
     } else {
-      this.filtersList[this.filter] = [this.min, this.max];
+      this.filtersList[this.filter] = [this.dispMin, this.dispMax];
     }
     this.scope_.$apply();
   }
@@ -176,9 +222,30 @@ app.SliderController.prototype.handleRangeChange_ = function() {
  * @private
  */
 app.SliderController.prototype.handleClear_ = function() {
-  this.min = this.boundaries[0];
-  this.max = this.boundaries[1];
+  this.updateMinMax_(this.boundaries);
   // FIXME make sure the slider handles are reset too
+};
+
+
+/**
+ * @param {Array.<number>} values Array of min/max values.
+ * @private
+ */
+app.SliderController.prototype.updateMinMax_ = function(values) {
+  this.min = values[0];
+  this.max = values[1];
+  this.dispMin = this.getDisp_(this.min);
+  this.dispMax = this.getDisp_(this.max);
+};
+
+
+/**
+ * @param {number} value Slider value.
+ * @return {number|string}
+ * @private
+ */
+app.SliderController.prototype.getDisp_ = function(value) {
+  return this.showStringValues_ ? this.valuesList[value] : value;
 };
 
 
