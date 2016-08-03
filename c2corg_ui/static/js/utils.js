25154f964368f02e57d00b5f04cd4755009da911
Adam Gina <adam.gina@camptocamp.com>
2016-08-03 13:48:50 +0200

    use small images for thumbnails

--- c2corg_ui/static/js/utils.js
+++ c2corg_ui/static/js/utils.js
@@ -119,23 +119,13 @@ app.utils.getImageFileBase64Source = function(file) {
  * @export
  */
 app.utils.createImageSlide = function(file, imageUrl) {
-  var img;
-  var ahref;
-  // if !src => it's an already existing image, else it's an image that has just been uploaded
-  if (!file['src']) {
-    ahref = '<a href="' + imageUrl + file.filename + '" data-info-id="' + file['document_id'] + '">';
-    img = '<img src="' + imageUrl + file.filename + '">';
-  } else {
-    ahref = '<a href="' + file['src'] + '" data-info-id="' + file['document_id'] + '">';
-    img = '<img src=" ' + file['src'] + '">';
-  }
-  return '<figure id="image-' + file['document_id'] + '">' +
-               ahref + img +
-               '</a>' +
-               '<app-slide-info></app-slide-info>' +
-             '</figure>';
-};
+  var smallImage = app.utils.createImageUrl(file.filename, 'SI');
+  var bigImage = app.utils.createImageUrl(file.filename, 'BI');
+  var ahref = '<a href="' + imageUrl + bigImage + '" data-info-id="' + file['image_id'] + '-slide">';
+  var img = '<img src="' + imageUrl + smallImage + '"></a>';
 
+  return '<figure id="' + file['image_id'] + '">' + ahref + img +  '<app-slide-info></app-slide-info></figure>';
+};
 
 /**
  * @param {string} url
