a0823822a7cc1960a426f671ac2c27e2424d0f94
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2016-10-19 15:28:00 +0200

    Rename users view to profiles

--- c2corg_ui/views/profile.py
+++ c2corg_ui/views/profile.py
@@ -8,12 +8,16 @@ from c2corg_ui.views.document import Document
 log = logging.getLogger(__name__)
 
 
-class User(Document):
+class Profile(Document):
 
-    _API_ROUTE = 'users'
+    _API_ROUTE = 'profiles'
 
-    @view_config(route_name='users_view',
-                 renderer='c2corg_ui:templates/user/view.html')
+    @view_config(route_name='profiles_index')
+    def index(self):
+        pass
+
+    @view_config(route_name='profiles_view',
+                 renderer='c2corg_ui:templates/profile/view.html')
     def detail(self):
         id, lang = self._validate_id_lang()
         self.template_input.update({
@@ -22,13 +26,12 @@ class User(Document):
         })
         return self.template_input
 
-    @view_config(route_name='users_view_id')
+    @view_config(route_name='profiles_view_id')
     def redirect_to_full_url(self):
         self._redirect_to_full_url()
 
-
-    @view_config(route_name='users_data',
-                 renderer='c2corg_ui:templates/user/data.html')
+    @view_config(route_name='profiles_data',
+                 renderer='c2corg_ui:templates/profile/data.html')
     def data(self):
         id, lang = self._validate_id_lang()
         headers = None
@@ -36,7 +39,7 @@ class User(Document):
             headers = {
                 'Authorization': self.request.headers.get('Authorization')
             }
-        url = 'profiles/%s' % str(id)
+        url = '%s/%d' % (self._API_ROUTE, id)
         resp, data = self._call_api(url, headers)
 
         if resp.status_code != 200 and resp.status_code != 404:
@@ -50,7 +53,7 @@ class User(Document):
         elif data.get('not_authorized', False):
             self.template_input.update({
                 'not_authorized': True,
-                'user': {'name': data['name']}
+                'profile': {'name': data['name']}
             })
         else:
             # get the locale in the desired lang if available
@@ -58,7 +61,7 @@ class User(Document):
             locales = [l for l in locales if l['lang'] == lang] or locales
             self.template_input.update({
                 'lang': locales[0]['lang'],
-                'user': data,
+                'profile': data,
                 'locale': locales[0],
                 'geometry': self._get_geometry(data['geometry']['geom']),
                 'version': None
