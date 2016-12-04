498867b97b741a9dc96587e218060ac90531b41c
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2016-12-04 01:05:03 +0100

    Add list of followed users

--- c2corg_ui/static/js/main.js
+++ c2corg_ui/static/js/main.js
@@ -24,6 +24,7 @@ goog.require('app.diffMapDirective');
 goog.require('app.documentEditingDirective');
 goog.require('app.feedDirective');
 goog.require('app.followDirective');
+goog.require('app.followingDirective');
 goog.require('app.geolocationDirective');
 goog.require('app.gpxUploadDirective');
 goog.require('app.imageUploaderDirective');
