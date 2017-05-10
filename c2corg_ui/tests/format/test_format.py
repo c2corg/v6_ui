4e9fd014d411c65b970bbf68824b131256e76f0f
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2017-05-10 23:27:17 +0200

    fix #1640

--- c2corg_ui/tests/format/test_format.py
+++ c2corg_ui/tests/format/test_format.py
@@ -2,16 +2,15 @@ import os
 import unittest
 import bbcode
 import markdown
+import json
 
 from c2corg_ui.format.wikilinks import C2CWikiLinkExtension
 from c2corg_ui.tests import read_file
-
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 
 
 class TestFormat(unittest.TestCase):
-
     def setUp(self):  # noqa
         extensions = [
             C2CWikiLinkExtension(),
@@ -20,25 +19,24 @@ class TestFormat(unittest.TestCase):
                                                  extensions=extensions)
         self.bbcode_parser = bbcode.Parser(escape_html=False, newline='\n')
 
-    def test_wikilinks(self):
-        input1 = (
-            'Some text and a [[waypoints/12345/fr/some-slug|wiki link]] '
-            'before [[/whatever|another one]] that follows.'
-        )
-        output1 = (
-            '<p>Some text and a '
-            '<a href="/waypoints/12345/fr/some-slug">wiki link</a> '
-            'before <a href="/whatever">another one</a> that follows.</p>'
-        )
-        md_output1 = self.markdown_parser.convert(input1)
-        self.assertEqual(md_output1, output1)
-        # Make sure the bbcode parser does not impact the wikilink conversion:
-        bb_output1 = self.bbcode_parser.format(md_output1)
-        self.assertEqual(bb_output1, output1)
+    def test_all(self):
+        def do_test(id, markdown, expected):
+            result = self.markdown_parser.convert(markdown)
+            result = self.bbcode_parser.format(result)
+            self.assertEqual(result, expected, id)
 
-    def test_full_conversion(self):
-        input_txt = read_file(os.path.join(base_path, 'input.txt'))
-        output_txt = read_file(os.path.join(base_path, 'output.txt'))
-        txt = self.markdown_parser.convert(input_txt)
-        txt = self.bbcode_parser.format(txt)
-        self.assertEqual(txt, output_txt)
+        test_path = os.path.join(base_path, 'test')
+
+        for file in os.listdir(test_path):
+            file_path = os.path.join(test_path, file)
+            if os.path.isfile(file_path):
+                if file.endswith(".md"):
+                    markdown = read_file(file_path)
+                    expected = read_file(file_path.replace(".md", ".html"))
+                    do_test(file, markdown, expected)
+
+                elif file.endswith(".json"):
+                    tests = json.loads(read_file(file_path))
+
+                    for test in tests:
+                        do_test(**test)
