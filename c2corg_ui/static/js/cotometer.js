e7d18c0c11f599ff5d62ce53b1edab512a2d44b6
Courgette <nicolas13008@gmail.com>
2017-05-14 10:58:11 +0200

    fix bug, change function name and fix travis error

--- c2corg_ui/static/js/cotometer.js
+++ c2corg_ui/static/js/cotometer.js
@@ -100,49 +100,48 @@ app.CotometerController = function(documentData, appApi, gettextCatalog, $uibMod
 
 /**
  * @private
- * @return {string}
  */
-app.CotometerController.prototype.cotometerRating= function() {
+app.CotometerController.prototype.cotometerRating = function() {
 
-  var inter = Math.tan(Math.PI * this.slope/180) + 0.1 * Math.log(this.elevation);
+  var inter = Math.tan(Math.PI * this.slope / 180) + 0.1 * Math.log(this.elevation);
   inter += this.skiability * (inter - 1);
-  if (inter <1.32) this.rating = "≤3.3";
-  if (inter >=1.32 & inter <1.42) this.rating = "4.1";
-  if (inter >=1.42 & inter <1.5) this.rating = "4.2";
-  if (inter >=1.5 & inter <1.575) this.rating = "4.3";
-  if (inter >=1.575 & inter <1.67) this.rating = "5.1";
-  if (inter >=1.67 & inter <1.745) this.rating = "5.2";
-  if (inter >=1.745 & inter <1.81) this.rating = "5.3";
-  if (inter >=1.81 & inter <1.95) this.rating = "5.4";
-  if (inter >=1.95 & inter <2.09) this.rating = "5.5";
-  if (inter >=2.09 & inter <2.25) this.rating = "5.6";
-  if (inter >=2.25 & inter <2.4) this.rating = "5.7";
-  return "≥5.8";
+  if (inter < 1.32) this.rating = '≤3.3';
+  if (inter >= 1.32 & inter < 1.42) this.rating = '4.1';
+  if (inter >= 1.42 & inter < 1.5) this.rating = '4.2';
+  if (inter >= 1.5 & inter < 1.575) this.rating = '4.3';
+  if (inter >= 1.575 & inter < 1.67) this.rating = '5.1';
+  if (inter >= 1.67 & inter < 1.745) this.rating = '5.2';
+  if (inter >= 1.745 & inter < 1.81) this.rating = '5.3';
+  if (inter >= 1.81 & inter < 1.95) this.rating = '5.4';
+  if (inter >= 1.95 & inter < 2.09) this.rating = '5.5';
+  if (inter >= 2.09 & inter < 2.25) this.rating = '5.6';
+  if (inter >= 2.25 & inter < 2.4) this.rating = '5.7';
+  this.rating = '5.8';
 };
 
 
 /**
  * @export
  */
-app.CotometerController.prototype.cotometreTechnicalGrade= function () {
+app.CotometerController.prototype.cotometerTechnicalGrade = function() {
 
-  if(isNaN(this.skiability)) {
-    alert("Vous devez choisir une skiabilité");
+  if (isNaN(this.skiability)) {
+    alert('Vous devez choisir une skiabilité');
     return false;
   }
 
   if (isNaN(this.slope) || this.slope < 20.0 || this.slope > 80.0) {
-    alert("La pente doit être comprise entre 20° et 80°");
+    alert('La pente doit être comprise entre 20° et 80°');
     return false;
   }
 
   if (isNaN(this.elevation) || this.elevation < 50.0 || this.elevation > 3000.0) {
-    alert("Le dénivelé doit être compris entre 50m et 3000m");
+    alert('Le dénivelé doit être compris entre 50m et 3000m');
     return false;
   }
 
   this.cotometerRating();
-}
+};
 
 
 /**
