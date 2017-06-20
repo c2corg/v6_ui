a634805e755652ce9eaa8ddf2ee6bd2f946e2ea8
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2017-06-20 13:17:53 +0200

    Show mountain biking ratings in route cards

--- c2corg_ui/static/js/constants.js
+++ c2corg_ui/static/js/constants.js
@@ -47,5 +47,6 @@ app.constants = {
   },
   fullRatingOrdered: ['global_rating', 'rock_rating', 'aid_rating', 'ice_rating', 'mixed_rating',
     'via_ferrata_rating', 'engagement_rating', 'risk_rating', 'equipment_rating', 'exposition_rock_rating',
-    'ski_rating', 'labande_rating', 'hiking_rating', 'snowshoe_rating', 'mtb_rating', 'hiking_mtb_exposition']
+    'ski_rating', 'labande_rating', 'hiking_rating', 'snowshoe_rating', 'mtb_up_rating',
+    'mtb_down_rating', 'hiking_mtb_exposition']
 };
