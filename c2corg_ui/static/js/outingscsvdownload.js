b3ede9031351b8cb450ba6954d80ddd8eda760ea
Bruno Besson <bruno.besson@gmail.com>
2017-06-26 12:33:02 +0200

    retrieve outings and build CSV file

--- c2corg_ui/static/js/outingscsvdownload.js
+++ c2corg_ui/static/js/outingscsvdownload.js
@@ -23,11 +23,41 @@ app.module.directive('appOutingsCsvDownload', app.outingsCsvDownloadDirective);
 
 
 /**
+ * @param {app.Authentication} appAuthentication
+ * @param {app.Api} appApi The API service
+ * @param {angular.$q} $q Angular q service.
+ * @param {app.Lang} appLang Lang service.
  * @constructor
  * @struct
  * @ngInject
  */
-app.OutingsCsvDownloadController = function() { };
+app.OutingsCsvDownloadController = function(appAuthentication, appApi, $q,
+    appLang) {
+
+  /**
+   * @type {app.Authentication}
+   * @export
+   */
+  this.auth = appAuthentication;
+
+  /**
+   * @type {app.Api}
+   * @export
+   */
+  this.appApi = appApi;
+
+  /**
+   * @type {angular.$q}
+   * @private
+   */
+  this.q_ = $q;
+
+  /**
+   * @type {app.Lang}
+   * @private
+   */
+  this.langService_ = appLang;
+};
 
 
 /**
@@ -37,23 +67,82 @@ app.OutingsCsvDownloadController = function() { };
 app.OutingsCsvDownloadController.prototype.download = function(event) {
   event.stopPropagation();
   var filename = 'outings.csv';
-  var csvFile = '"toto", "tata"\n"1", "2"';
-  var blob = new Blob([csvFile], {type: 'text/csv;charset=utf-8;'});
-  if (window.navigator.msSaveBlob) { // IE 10+
-    window.navigator.msSaveBlob(blob, filename);
-  } else {
-    var link = document.createElement('a');
-    if (link.download !== undefined) { // feature detection
-      // Browsers that support HTML5 download attribute
-      var url = URL.createObjectURL(blob);
-      link.setAttribute('href', url);
-      link.setAttribute('download', filename);
-      link.style.visibility = 'hidden';
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
+  var langService = this.langService_;
+  this.getOutings_().then(function(outings) {
+    var csvFile = '';
+    outings.forEach(function(outing) {
+      var cells = [
+        outing.date_start,
+        outing.locales[0].title,
+        outing.activities.map(function(activity) {
+          return langService.translate(activity);
+        }).join(';'),
+        'https://www.camptocamp.org/outings/' + outing.document_id
+      ];
+      csvFile += '"' + cells.map(function(cell) {
+        if (cell.indexOf('"') > -1) {
+          return cells.replace(/"/g,'""');
+        }
+        return cell;
+      }).join('","') + '"\n';
+    });
+    var blob = new Blob([csvFile], {type: 'text/csv;charset=utf-8;'});
+    if (window.navigator.msSaveBlob) { // IE 10+
+      window.navigator.msSaveBlob(blob, filename);
+    } else {
+      var link = document.createElement('a');
+      if (link.download !== undefined) { // feature detection
+        // Browsers that support HTML5 download attribute
+        var url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.setAttribute('download', filename);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
     }
-  }
+  });
+};
+
+
+/**
+ * @private
+ * @returns {!angular.$q.Promise}
+ */
+app.OutingsCsvDownloadController.prototype.getOutings_ = function() {
+  var promise = this.q_.defer().promise; // a promise that does... nothing
+  var userId = this.auth.userData.id;
+  var appApi = this.appApi;
+  var $q = this.q_;
+  return appApi.listDocuments('outings', 'u=' + userId, promise)
+    .then(function(resp) {
+      var total = resp.data.total;
+      var outings = resp.data.documents.slice();
+      if (total > resp.data.documents.length) {
+        // retrieve all other outings
+        var offsets = Array.apply(null, Array(Math.floor(total / 30)))
+          .map(function(value, index) {
+            return 30 * (index + 1);
+          });
+        var additionalRequests = [];
+        offsets.forEach(function(offset) {
+          additionalRequests.push(appApi.listDocuments('outings',
+              'u=' + userId + '&offset=' + offset, promise));
+        });
+        return $q.all(additionalRequests).then(function(results) {
+          results.forEach(function(result) {
+            outings = outings.concat(result.data.documents)
+              .sort(function(o1, o2) {
+                return Date.parse(o1.date_start) - Date.parse(o2.date_start);
+              });
+          });
+          return outings;
+        });
+      } else {
+        return outings;
+      }
+    });
 };
 
 
