f6963cc932d17b65e0f61f63922830a0c48d0c2a
desnoes <mathieu.desnoes@free.fr>
2018-03-29 08:10:31 +0200

    preset quality for outings, articles and images Modification (reorganization of code, change var by let...)

--- c2corg_ui/static/js/editing/articleediting.js
+++ c2corg_ui/static/js/editing/articleediting.js
@@ -53,4 +53,47 @@ app.ArticleEditingController.prototype.filterData = function(data) {
   return data;
 };
 
+/**
+ * @param {appx.Document} doc Document attributes.
+ * @return {number}
+ * @override
+ */
+app.ArticleEditingController.prototype.presetQuality = function(doc) {
+  let score = 0;
+
+  if ('summary' in doc.locales[0] && doc.locales[0]['summary']) {
+    score++;
+  }
+
+  if (doc['associations']['waypoints'].length) {
+    score++;
+  }
+
+  if (doc['associations']['images'].length) {
+    score++;
+  }
+
+  if ('description' in doc.locales[0] && doc.locales[0]['description']) {
+    score++;
+
+    const pattern_title = /(^|\n)##/g; // regex for title
+    const result_search_title = doc['locales'][0]['description'].search(pattern_title);
+    if (result_search_title !== -1) {
+      score++;
+    }
+
+    const pattern_img = /\[img=/g; // regex for image
+    const result_search_img = doc['locales'][0]['description'].search(pattern_img);
+    if (result_search_img !== -1) {
+      score++;
+    }
+
+  } else {
+    score = Math.min(score, 1);
+  }
+
+  score = Math.min(score, 4);
+  return score;
+};
+
 app.module.controller('appArticleEditingController', app.ArticleEditingController);
