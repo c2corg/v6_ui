94e478505c0b7f89d6c4a7628fa92d1e728bd8bb
Bruno Besson <bruno.besson@gmail.com>
2018-07-27 14:11:19 +0200

    also define corejs-typahead as external

--- webpack.common.js
+++ webpack.common.js
@@ -36,5 +36,5 @@ module.exports = {
       'goog/asserts.js': path.resolve(__dirname, 'node_modules/ngeo/src/goog.asserts.js')
     }
   },
-  externals: /^(jquery|\$|moment)$/i
+  externals: /^(jquery|\$|moment|corejs-typeahead)$/i
 };
